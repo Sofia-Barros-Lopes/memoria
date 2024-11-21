c6e264a2bcc0a9ae45cd593f85ad1eefdbd01b58
Sofia Lopes <sofia.barros@aluno.ifsp.edu.br>
2024-11-21 15:19:42 -0300

    alterar cor de fundo

--- src/main/java/br/edu/ifsp/PrimaryController.java
+++ src/main/java/br/edu/ifsp/PrimaryController.java
@@ -1,12 +1,32 @@
 package br.edu.ifsp;
 
 import java.io.IOException;
-import javafx.fxml.FXML;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class PrimaryController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
+public class PrimaryController implements Initializable {
+
+    @FXML
+    private VBox vboxPrincipal;
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        BackgroundFill bf = new BackgroundFill(Color.AQUAMARINE, CornerRadii.EMPTY, Insets.EMPTY);
+        Background bg = new Background(bf);
+        vboxPrincipal.setBackground(bg);
     }
 }
