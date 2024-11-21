2557802cac8d519a441dfc285c00ae84366719f1
Sofia Lopes <sofia.barros@aluno.ifsp.edu.br>
2024-11-21 15:45:19 -0300

    criando as cartas

--- src/main/java/br/edu/ifsp/App.java
+++ src/main/java/br/edu/ifsp/App.java
@@ -1,4 +1,4 @@
-package br.edu.ifsp;
+    package br.edu.ifsp;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
--- src/main/java/br/edu/ifsp/PrimaryController.java
+++ src/main/java/br/edu/ifsp/PrimaryController.java
@@ -7,6 +7,7 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
@@ -15,8 +16,13 @@ import javafx.scene.paint.Color;
 
 public class PrimaryController implements Initializable {
 
+    private boolean clicou = false;
+
     @FXML
     private VBox vboxPrincipal;
+
+    @FXML
+    private Button b11;
 
     @FXML
     private void switchToSecondary() throws IOException {
@@ -28,5 +34,21 @@ public class PrimaryController implements Initializable {
         BackgroundFill bf = new BackgroundFill(Color.AQUAMARINE, CornerRadii.EMPTY, Insets.EMPTY);
         Background bg = new Background(bf);
         vboxPrincipal.setBackground(bg);
+       // bf = new BackgroundFill(Color.YELLOW, CornerRadii.EMPTY, Insets.EMPTY);
+        //b11.setBackground(bg);
+    }
+
+    @FXML
+    public void revelar(){
+        BackgroundFill bf = null;
+        if (!clicou){
+            bf = new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY);
+            clicou = true;
+        } else {
+            bf = new BackgroundFill(Color.YELLOW, CornerRadii.EMPTY, Insets.EMPTY);
+            clicou = false;
+        }
+        Background bg = new Background(bf);
+        b11.setBackground(bg);
     }
 }
