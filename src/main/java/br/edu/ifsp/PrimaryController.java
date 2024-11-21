75bd7f662d43d2241c703e65a28cc26546054133
Sofia Lopes <sofia.barros@aluno.ifsp.edu.br>
2024-11-21 16:44:04 -0300

    realizar testes com imagens

--- src/main/java/br/edu/ifsp/PrimaryController.java
+++ src/main/java/br/edu/ifsp/PrimaryController.java
@@ -1,18 +1,21 @@
 package br.edu.ifsp;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class PrimaryController implements Initializable {
 
@@ -25,6 +28,9 @@ public class PrimaryController implements Initializable {
     private Button b11;
 
     @FXML
+    private ImageView ivImagem;
+
+    @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
@@ -34,18 +40,25 @@ public class PrimaryController implements Initializable {
         BackgroundFill bf = new BackgroundFill(Color.AQUAMARINE, CornerRadii.EMPTY, Insets.EMPTY);
         Background bg = new Background(bf);
         vboxPrincipal.setBackground(bg);
-       // bf = new BackgroundFill(Color.YELLOW, CornerRadii.EMPTY, Insets.EMPTY);
-        //b11.setBackground(bg);
     }
 
     @FXML
     public void revelar(){
         BackgroundFill bf = null;
         if (!clicou){
-            bf = new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY);
+
+            // lendo a imagem
+            Image image = new Image(this.getClass().getResource("animal-para-desenhar-2.jpg").toString());
+            System.out.println(image.getHeight() + " x " + image.getWidth());
+            System.out.println(image.getUrl());
+
+            // carregar o background no botão
+            ivImagem.setImage(image);
+
             clicou = true;
         } else {
             bf = new BackgroundFill(Color.YELLOW, CornerRadii.EMPTY, Insets.EMPTY);
+            ivImagem.setImage(null);
             clicou = false;
         }
         Background bg = new Background(bf);
